8b5bb0cb4b29c674af52c0837c2ca0447956280e
ubermanu <manu@paca.happy-dev.fr>
2023-09-16 16:26:15 +0200

    test change types

--- test/compare.test.ts
+++ test/compare.test.ts
@@ -26,6 +26,7 @@ test('detect changes if a property is missing', (t) => {
   const css1 = `
     .foo {
       color: red;
+      font-size: 16px;
     }
   `
 
@@ -35,5 +36,47 @@ test('detect changes if a property is missing', (t) => {
     }
   `
 
-  t.is(compare(css1, css2, fooHtml).equal, true)
+  const result = compare(css1, css2, fooHtml)
+  t.is(result.equal, false)
+  t.is(result.changes.length, 1)
+  t.is(result.changes[0].type, 'missing')
+})
+
+test('detect changes if a property is different', (t) => {
+  const css1 = `
+    .foo {
+      font-size: 16px;
+    }
+  `
+
+  const css2 = `
+    .foo {
+      font-size: 20px;
+    }
+  `
+
+  const result = compare(css1, css2, fooHtml)
+  t.is(result.equal, false)
+  t.is(result.changes.length, 1)
+  t.is(result.changes[0].type, 'different')
+})
+
+test('detect changes if a property is extra', (t) => {
+  const css1 = `
+    .foo {
+      color: red;
+    }
+  `
+
+  const css2 = `
+    .foo {
+      color: red;
+      font-size: 16px;
+    }
+  `
+
+  const result = compare(css1, css2, fooHtml)
+  t.is(result.equal, false)
+  t.is(result.changes.length, 1)
+  t.is(result.changes[0].type, 'extra')
 })
