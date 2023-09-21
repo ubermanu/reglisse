846a68f7f7865fc1398aba674bacd9aac62f5b59
ubermanu <manu@paca.happy-dev.fr>
2023-09-21 10:51:05 +0200

    add viewport option

--- src/puppet.ts
+++ src/puppet.ts
@@ -1,22 +1,28 @@
-import puppeteer, { Browser, Page } from 'puppeteer'
+import puppeteer, { Browser, Page, Viewport } from 'puppeteer'
 
 // Create a new puppeteer instance and inject css + html
 // TODO: Remove CSS and script tags from the document
 export async function createPuppet(
   html: string,
-  css: string
+  css: string,
+  viewport?: Viewport
 ): Promise<{ page: Page; browser: Browser }> {
   const browser = await puppeteer.launch({
     headless: true,
     devtools: false,
     args: ['--no-sandbox', '--disable-setuid-sandbox', '--no-zygote'],
   })
+
   const page = await browser.newPage()
 
   await page.setContent(html || '<html lang></html>')
 
   if (typeof css === 'string' && css.length > 0) {
     await page.addStyleTag({ content: css })
+  }
+
+  if (viewport) {
+    await page.setViewport(viewport)
   }
 
   return { page, browser }
